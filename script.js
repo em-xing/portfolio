4f6ddaa089132d24ece56ff331b5b5a9672c0111
em-xing <exing@college.harvard.edu>
2024-12-20 14:22:52 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const body = document.body;
-    const scrollY = window.scrollY;
-    const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
-    const percentage = scrollY / maxScroll;
-
-    const hue = Math.round(percentage * 360); // Full color wheel rotation
-    body.style.background = `linear-gradient(to bottom, hsl(${hue}, 80%, 50%), hsl(${(hue + 60) % 360}, 80%, 50%))`;
-});
